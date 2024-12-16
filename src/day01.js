2b48d16f0cdc4e355d66c09af6e0a1c0eacd1bf0
William Ortiz <dandelius@gmail.com>
2024-12-15 20:26:38 -0500

    Revert "Código en JS"
    This reverts commit 91155a158dd53163ca69293ac62ce1bef6a1bb17.

--- src/day01.js
+++
@@ -1,9 +0,0 @@
-export const prepareGifts = ( gifts = [] ) => {
-  if ( !Array.isArray( gifts ) || !gifts.every( n => typeof n === 'number' ) ) {
-    throw new TypeError('Debe ser un array de enteros')
-  }
-  const newArray = [... new Set(gifts)]
-  return newArray.sort((a,b)=> a - b)
-}
-
-
